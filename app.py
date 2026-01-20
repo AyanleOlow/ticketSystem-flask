a3a0487bb5cd8e6ea5394dcc937f250fa8f1ebf0
Ayanle Olow <randomrandomo156@gmail.com>
2026-01-20 11:43:12 +0100

    lagde en prototype av login som fungerer ikke og navbar

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-import flask
-
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("homepage.html")
+
+@app.route("/auth")
+def auth():
+    return render_template("auth.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
